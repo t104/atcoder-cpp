335828f45f8594feb9d8179282b1ebcf1d700d76
Toshiki Takei <toshiki.takei@avanade.com>
2020-09-27 23:40:58 +0900

    abc084-d

---
+++ abc084/d.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T> void chmax(T &a, T b) { a = max(a, b); }
+template<typename T> void chmin(T &a, T b) { a = min(a, b); }
+using ll = long long;
+using P = pair<int,int>;
+
+// Sieve of Eratosthenes
+// https://youtu.be/UTVg7wzMWQc?t=2774
+struct Sieve {
+    int n;
+    vector<int> f, primes;
+    Sieve(int n=1):n(n), f(n+1) {
+        f[0] = f[1] = -1;
+    for (ll i = 2; i <= n; ++i) {
+      if (f[i]) continue;
+      primes.push_back(i);
+      f[i] = i;
+      for (ll j = i*i; j <= n; j += i) {
+        if (!f[j]) f[j] = i;
+      }
+    }
+  }
+  bool isPrime(int x) { return f[x] == x;}
+  vector<int> factorList(int x) {
+    vector<int> res;
+    while (x != 1) {
+      res.push_back(f[x]);
+      x /= f[x];
+    }
+    return res;
+  }
+  vector<P> factor(int x) {
+    vector<int> fl = factorList(x);
+    if (fl.size() == 0) return {};
+    vector<P> res(1, P(fl[0], 0));
+    for (int p : fl) {
+      if (res.back().first == p) {
+        res.back().second++;
+      } else {
+        res.emplace_back(p, 1);
+      }
+    }
+    return res;
+  }
+  vector<pair<ll,int>> factor(ll x) {
+    vector<pair<ll,int>> res;
+    for (int p : primes) {
+      int y = 0;
+      while (x%p == 0) x /= p, ++y;
+      if (y != 0) res.emplace_back(p,y);
+    }
+    if (x != 1) res.emplace_back(x,1);
+    return res;
+  }
+};
+
+const int MAX_N = 100005;
+int op (int x, int y) {
+    return x + y;
+}
+
+int e () {
+    return 0;
+}
+
+int main() {
+    int q;
+    cin >> q;
+    Sieve s(MAX_N);
+    segtree<int, op, e> seg(MAX_N);
+
+    for (int p : s.primes) {
+        int q = (p+1) / 2;
+        if (s.isPrime(q)) seg.set(p, 1);
+    }
+
+    rep(i, q) {
+        int l, r;
+        cin >> l >> r;
+        cout << seg.prod(l, r + 1) << endl;
+    }
+    return 0;
+}
