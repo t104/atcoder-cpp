14f7af956c672f362b752d0b18d03e3cfcc230c4
Toshiki Takei <toshiki.takei@avanade.com>
2020-09-27 22:46:29 +0900

    abc147-c

--- abc147/c.cpp
+++ abc147/c.cpp
@@ -7,7 +7,8 @@ using P = pair<int,int>;
 int n;
 
 bool trust(int flag, int index) {
-    return (flag >> index) & 1;
+    bool trust = (flag >> index) & 1;
+    return trust;
 }
 
 int count (int flag) {
@@ -27,22 +28,23 @@ int main() {
         evidence.at(i).resize(a);
         rep(j, a) {
             cin >> evidence.at(i).at(j).first >> evidence.at(i).at(j).second;
+            evidence.at(i).at(j).first--;
         }
     }
     int ans = 0;
-    for (int flag = 0; flag <= 1<<n; flag++) {
+    for (int flag = 0; flag < (1<<n); flag++) {
         bool ok = true;
         rep(i, n) {
             if (trust(flag, i)) {
                 for (P p : evidence.at(i)) {
-                    if (trust(flag, p.first) != p.second) {
+                    if (trust(flag, p.first) ^ (p.second == 1)) {
                         ok = false;
                         break;
                     }
                 }
             }
         }
-        ans = max(ans, count(flag));
+        if (ok) ans = max(ans, count(flag));
     }
     cout << ans << endl;
     return 0;
