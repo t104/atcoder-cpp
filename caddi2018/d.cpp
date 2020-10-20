837f6efcc8e8853e97f7d60616f2fbf48c0d7f45
Toshiki Takei <toshiki.takei@avanade.com>
2020-10-20 23:38:36 +0900

    caddi2018-d

---
+++ caddi2018/d.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i,n) cin >> a.at(i);
+    for (int ai : a) {
+        if (ai%2 == 1) {
+            cout << "first" << endl;
+            return 0;
+        }
+    }
+    cout << "second" << endl;
+    return 0;
+}
