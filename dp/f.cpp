332355e4b8875987d30bcbad14039010225cdb76
Toshiki Takei <toshiki.takei@avanade.com>
2020-11-10 08:53:30 +0900

    dp-f

--- dp/f.cpp
+++ dp/f.cpp
@@ -13,12 +13,30 @@ int main() {
     cin >> s >> t;
     int len_s = s.size(), len_t = t.size();
     vector<vector<int>> dp(len_s + 1, vector<int>(len_t + 1));
-    for (int i = 1; i <= len_s; ++i) {
-        for (int j = 1; j <= len_t; ++j) {
-            if (s.at(i) == t.at(i))
-                dp.at(i).at(j) = dp.at(i-1).at(j-1) + 1;
-            else dp.at(i).at(j) = max(dp.at(i-1).at(j), dp.at(i).at(j-1));
+    for (int i = 0; i < len_s; ++i) {
+        for (int j = 0; j < len_t; ++j) {
+            if (s.at(i) == t.at(j))
+                dp.at(i+1).at(j+1) = dp.at(i).at(j) + 1;
+            else {
+                dp.at(i+1).at(j+1) = max(dp.at(i).at(j+1), dp.at(i+1).at(j));
+            }
         }
     }
+    string ans;
+    int len = dp.at(len_s).at(len_t);
+    ans.resize(len);
+    int i = len_s - 1, j = len_t - 1;
+    len--;
+    while (0 <= len) {
+        if (s.at(i) == t.at(j)) {
+            ans.at(len) = s.at(i);
+            --i;
+            --j;
+            --len;
+        }
+        else if (dp.at(i+1).at(j+1) == dp.at(i).at(j+1)) --i;
+        else --j;
+    }
+    cout << ans << endl;
     return 0;
 }
