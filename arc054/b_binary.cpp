14a6030d94a479f5ae022455c27fcbf318788f60
Toshiki Takei <toshiki.takei@avanade.com>
2020-12-08 19:58:06 +0900

    arc054-b

--- arc054/b_binary.cpp
+++ arc054/b_binary.cpp
@@ -9,22 +9,37 @@ using ll = long long;
 using P = pair<int,int>;
 const int INF = 1001001001;
 const ll LINF = 1LL<<60;
-
 double p;
 
+double df(double x) {
+    return 1 + p * (pow(2, -2./3. * x) * (-2./3. * log(2)));
+}
+
 double f(double x) {
-    return x + p * pow(2, -x/1.5);
+    return x + p * pow(2, -2./3. * x);
 }
 
 int main() {
     cout << std::fixed << std::setprecision(15);
     cin >> p;
-    double x = 1.5 * (1 - log2(3 / (p * log(2))));
-    cout << x << endl;
-    if (0 < x) {
-        cout << f(x) << endl;
+    double l = -1e18, r = 1e18;
+    double x = 0.;
+    rep(i,10000) {
+        double mid = (l+r) / 2;
+        if (0 < df(mid)) {
+            r = mid;
+            x = mid;
+        }
+        else {
+            l = mid;
+            x = mid;
+        }
+    }
+    // cout << df(x) << endl;
+    if (x < 0) {
+        cout << f(0) << endl;
         return 0;
     }
-    cout << f(0.0) << endl;
+    cout << f(x) << endl;
     return 0;
 }
