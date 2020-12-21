1067eacc9f5ca899e327d48544035ef02d926b3d
Toshiki Takei <toshi2005tke@gmail.com>
2020-12-22 08:05:56 +0900

    merge

---
+++ abc186/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using P = pair<int,int>;
+const int INF = 1001001001;
+const ll LINF = 1LL<<60;
+
+int main() {
+    int n, w;
+    cin >> n >> w;
+    cout << n/w << endl;
+    return 0;
+}
---
+++ abc186/d_.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using P = pair<int,int>;
+const int INF = 1001001001;
+const ll LINF = 1LL<<60;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i,n) cin >> a.at(i);
+    ll sum = 0;
+    rep(i,n) {
+        for (int j = i+1; j < n; ++j) {
+            sum += abs(a.at(i) - a.at(j));
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
