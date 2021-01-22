74a1bc8e7665e57a28e3c4457f905576d0ba509d
t104 <toshi2005tke@gmail.com>
2021-01-22 22:50:22 +0900

    abc149

---
+++ abc149/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using P = pair<int,int>;
+
+int main() {
+    ll a, b, k;
+    cin >> a >> b >> k;
+    ll d = min(a, k);
+    a -= d;
+    k -= d;
+    d = min(b, k);
+    b -= d;
+    cout << a << ' ' << b << endl;
+    return 0;
+}
