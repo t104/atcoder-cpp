9256e98f9ed1e238aad34d672d3b171c057c9d4a
t104 <toshi2005tke@gmail.com>
2021-02-08 10:18:06 +0900

    abc165-c

--- abc165/c.cpp
+++ abc165/c.cpp
@@ -1,38 +1,49 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
 using ll = long long;
+using P = pair<int,int>;
 
-int a[55], b[55], c[55], d[55];
+int n, m, q;
+
+vector<vector<int>> As;
+
+void dfs(vector<int> A = {1}) {
+    if (A.size() == n+1) {
+        As.push_back(A);
+        return;
+    }
+    for (int i = A.back(); i <= m; ++i) {
+        vector<int> next = A;
+        next.push_back(i);
+        dfs(next);
+    }
+}
 
 int main() {
-    int n, m, q;
     cin >> n >> m >> q;
-    rep(i, q) {
-        cin >> a[i] >> b[i] >> c[i] >> d[i];
+    vector<int> a(q), b(q), c(q), d(q);
+    rep(i,q) cin >> a[i] >> b[i] >> c[i] >> d[i];
+    dfs();
+
+    int ans = 0;
+
+    for (auto A: As) {
+        int cnt = 0;
+        rep(i,q) {
+            if (A[b[i]] - A[a[i]] == c[i]) cnt += d[i];
+        }
+        chmax(ans, cnt);
     }
-    ll ans = 0;
-    vector<int> comb(m + n);
-    rep(i, m) comb.at(i) = 1;
-    reverse(comb.begin(), comb.end());
-    do {
-        vector<int> A;
-        int k = 0;
-        rep(i, n + m) {
-            if(comb.at(i) == 0) A.push_back(k);
-            else {
-                k++;
-            }
-        }
-        ll sum = 0;
-        rep(i, q) {
-            if(A.at(b[i] - 1) - A.at(a[i] - 1) == c[i]) {
-                sum += d[i];
-            }
-        }
-        ans = max(ans, sum);
-    } while(next_permutation(comb.begin(), comb.end()));
 
     cout << ans << endl;
+
     return 0;
 }
