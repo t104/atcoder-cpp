54dc922343e7e16aeb3d8befdc24958e0b284920
t104 <toshi2005tke@gmail.com>
2021-02-16 00:13:13 +0900

    abc163d

--- abc163/d.cpp
+++ abc163/d.cpp
@@ -1,12 +1,28 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
 using ll = long long;
-const int q = pow(10,9) + 7;
+using ld = long double;
+using P = pair<int,int>;
+
+using mint = modint1000000007;
 
 int main() {
-    int n,k;
+    int n, k;
     cin >> n >> k;
-    
+    mint ans = 0;
+    for (int i = k; i <= n+1; ++i) {
+        ll mi = (ll) i*(i-1)/2;
+        ll mx = (ll) (2*n-i+1)*i/2;
+        ans += mx - mi + 1;
+    }
+    cout << ans.val() << endl;
     return 0;
 }
