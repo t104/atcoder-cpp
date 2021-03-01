e7bd59f82b14eab9a114f98b466966cf460a6825
t104 <toshi2005tke@gmail.com>
2021-03-01 20:52:51 +0900

    abc167

--- abc167/e.cpp
+++ abc167/e.cpp
@@ -1,29 +1,48 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
 using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
 
-const ll MOD = 998244353;
+using mint = modint998244353;
 
-ll choose(ll x, int k, int l) {
-    
-}
+// combination mod prime
+// https://www.youtube.com/watch?v=8uowVvQ_-Mo&feature=youtu.be&t=1619
+const int MAX_N = 1000005;
+struct combination {
+  vector<mint> fact, ifact;
+  combination(int n):fact(n+1),ifact(n+1) {
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+  }
+  mint operator()(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n]*ifact[k]*ifact[n-k];
+  }
+  mint p(int n, int k) {
+    return fact[n]*ifact[n-k];
+  }
+} c(MAX_N);
 
 int main() {
     int n, m, k;
     cin >> n >> m >> k;
-    ll ans = 1;
-    ans *= m;
-    ans %= MOD;
-    rep(i, n-1) {
-        ans *= m - 1;
-        ans %= MOD;
+    mint mm = m;
+    mint mn = n;
+    mint ans = 0;
+    for (int i = 0; i <= k; ++i) {
+        ans += mm*(mm-1).pow(n-1-i)*c(n-1,i);
     }
-    ans += m;
-    ans %= MOD;
-    rep(i, n-1-k) {
-
-    }
-    cout << ans << endl;
+    cout << ans.val() << endl;
     return 0;
 }
