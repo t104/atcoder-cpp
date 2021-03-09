0c87a9d7a167b6d64397e6fd25ffabb9d0c936ee
t104 <toshi2005tke@gmail.com>
2021-03-09 21:34:43 +0900

    arc032-b

---
+++ arc032/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    dsu d(n);
+    rep(i,m) {
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+        d.merge(a,b);
+    }
+    cout << d.groups().size() - 1 << endl;
+    return 0;
+}
