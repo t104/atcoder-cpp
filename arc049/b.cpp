73a53c7c7437d8261e4ff298d9dea47c9a4577a6
t104 <toshi2005tke@gmail.com>
2021-03-21 19:47:27 +0900

    arc049

---
+++ arc049/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+
+
+int main() {
+    int n;
+    vector<int> x(n), y(n), c(n);
+    rep(i,n) cin >> x[i] >> y[i] >> c[i];
+
+    return 0;
+}
