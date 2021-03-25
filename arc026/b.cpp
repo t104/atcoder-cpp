57ec7f627f079ec2dc8230dd1ce7223c75945b0e
t104 <toshi2005tke@gmail.com>
2021-03-25 21:11:30 +0900

    arc026-b

---
+++ arc026/b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+int main() {
+    ll n;
+    cin >> n;
+    ll sum = 0;
+    for (ll i = 1; i*i <= n; ++i) {
+        if (n%i == 0) {
+            sum += i;
+            if (i != n/i) sum += n/i;
+        }
+    }
+    
+    sum -= n;
+    // cout << sum << endl;
+    if (n == sum) cout << "Perfect" << endl;
+    else if (sum < n) cout << "Deficient" << endl;
+    else cout << "Abundant" << endl;
+    return 0;
+}
