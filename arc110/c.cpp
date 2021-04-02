4f75ce1e8ffb536a2d6633039dfca7c65b6d76d6
t104 <toshi2005tke@gmail.com>
2021-04-02 23:27:13 +0900

    arc110-c

--- arc110/c.cpp
+++ arc110/c.cpp
@@ -2,53 +2,63 @@
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
 #define rep(i,n) for (int i = 0; i < (n); ++i)
-template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
-template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
 using ll = long long;
+using ld = long double;
 using P = pair<int,int>;
-const int INF = 1001001001;
-const ll LINF = 1LL<<60;
+
+void dame() {
+    cout << -1 << endl;
+    exit(0);
+}
 
 int main() {
     int n;
     cin >> n;
-    list<int> p(n);
-    for (auto itr = p.begin(); itr != p.end(); ++itr) {
-        cin >> *itr;
-        (*itr)--;
+    vector<int> p(n);
+    rep(i,n) cin >> p[i];
+    vector<int> sp(n);
+    vector<int> q(n);
+    rep(i,n) {
+        p[i]--;
+        q[p[i]] = i;
     }
-    vector<bool> use(n, false);
-    vector<int> ans;
-    for (auto itr = p.begin(); itr != p.end(); ++itr) {
-        int idx = distance(p.begin(), itr);
-        if (*itr == idx) continue;
-        for (auto j = itr; j != p.end(); ++j) {
-            int idxj = distance(p.begin(), j);
 
-            if(*j == idx) {
-                int x = *j;
-                p.erase(j);
-                p.insert(itr, x);
-                for (int k = idxj-1; idx <= k ; --k) {
-                    if (use.at(idxj)) {
-                        cout << -1 << endl;
-                        return 0;
-                    }
-                    use.at(idxj) = true;
-                    ans.push_back(k);
-                }
+    vector<int> ans;
+    rep(i,n) {
+        int d = q[i] - i;
+        int tmp = i;
+        if (0 < d) {
+            for (int j = q[i]; i < j; --j) {
+                if (sp[j-1]) dame();
+                ans.push_back(j-1);
+                sp[j-1]++;
+                swap(q[tmp], q[p[j-1]]);
+                swap(p[j],p[j-1]);
+            }
+        }
+        else {
+            for (int j = q[i]; j < i; ++j) {
+                if (sp[j]) dame();
+                ans.push_back(j);
+                sp[j]++;
+                swap(q[j], q[p[j+1]]);
+                swap(p[j], p[j+1]);
             }
         }
     }
 
-    if (ans.size() == n-1) {
-        rep(i,n-1) {
-            cout << ans.at(i) + 1 << endl;
-        }
-        return 0;
+    rep(i,n-1) {
+        if (sp[i] != 1) dame();
     }
 
-    cout << -1 << endl;
+    for (auto ai: ans) {
+        cout << ai+1 << endl;
+    }
     return 0;
 }
