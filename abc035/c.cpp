f2bacefff5c5a5bb527fddacebdaba34c28e17f5
t104 <toshi2005tke@gmail.com>
2021-04-05 14:11:24 +0900

    abc035-c

---
+++ abc035/c.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n+1);
+    rep(i,q) {
+        int l, r;
+        cin >> l >> r;
+        --l, --r;
+        a[l]++;
+        a[r+1]--;
+    }
+    rep(i,n) a[i+1] += a[i];
+    rep(i,n) cout << (a[i]%2);
+    cout << endl;
+    return 0;
+}
