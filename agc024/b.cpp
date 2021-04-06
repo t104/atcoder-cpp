c5baa89fc082d73fa57ce1428baa7a42dcdb6ca8
t104 <toshi2005tke@gmail.com>
2021-04-06 16:00:08 +0900

    agc024-b

---
+++ agc024/b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n), p(n);
+    rep(i,n) {
+        cin >> a[i];
+        a[i]--;
+        p[a[i]] = i;
+    }
+    int cnt = 1, mx = 0;
+    rep(i,n-1) {
+        if (p[i] < p[i+1]) cnt++;
+        else {
+            chmax(mx, cnt);
+            cnt = 1;
+        }
+    }
+    chmax(mx, cnt);
+    cout << n-mx << endl;
+    return 0;
+}
