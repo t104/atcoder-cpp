d9ebf6989bfd28b722c3da1df1e5aab4e6fe88d2
t104 <toshi2005tke@gmail.com>
2021-05-09 18:42:24 +0900

    solved

---
+++ tenkei90/004.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+    vector<vector<int>> a(h, vector<int>(w));
+    rep(i,h) rep(j,w) cin >> a[i][j];
+    vector<int> sh(h), sw(w);
+    rep(i,h) rep(j,w) {
+        sh[i] += a[i][j];
+    }
+    rep(i,w) rep(j,h) {
+        sw[i] += a[j][i];
+    }
+    rep(i,h) {
+        rep(j,w) {
+            int ans = sh[i] + sw[j] - a[i][j];
+            cout << ans << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
