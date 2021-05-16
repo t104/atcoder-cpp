d96026a756fd45ea7dad670be0d77150f449e81e
t104 <toshi2005tke@gmail.com>
2021-05-16 16:15:38 +0900

    solved

--- abc201/e.cpp
+++ abc201/e.cpp
@@ -12,6 +12,49 @@ using ll = long long;
 using ld = long double;
 using P = pair<int,int>;
 
+using mint = modint1000000007;
+
+struct Edge{
+    int to;
+    ll w;
+    Edge(int to, ll w): to(to), w(w) {}
+};
+
+int n;
+vector<vector<Edge>> g;
+
+void dfs(int v, int i, int w, vector<int> &dist) {
+    dist[v] = w;
+
+    for (auto e: g[v]) {
+        int nw = w;
+        if ((e.w>>i)&1) nw = (nw+1) % 2;
+        if (2 < dist[e.to]) {
+            dfs(e.to, i, nw, dist);
+        }
+    }
+}
+
 int main() {
+    cin >> n;
+    g.resize(n);
+    rep(i,n-1) {
+        int u, v;
+        ll w;
+        cin >> u >> v >> w;
+        --u, --v;
+        g[u].emplace_back(v, w);
+        g[v].emplace_back(u, w);
+    }
+
+    mint ans = 0;
+    rep(i,61) {
+        vector<int> dist(n, INF);
+        dfs(0, i, 0, dist);
+        int cnt = 0;
+        for (auto di: dist) cnt += di;
+        ans += mint(2).pow(i) * cnt * (n-cnt);
+    }
+    cout << ans.val() << endl;
     return 0;
 }
