ae59d3a41910a52491b5c10e397ebed94dde82d6
t104 <toshi2005tke@gmail.com>
2021-06-26 15:29:14 +0900

    7th try

--- ahc004/a.cpp
+++ ahc004/a.cpp
@@ -13,61 +13,127 @@ using ld = long double;
 using P = pair<string,int>;
 
 const int SZ = 20;
+clock_t start;
+const clock_t LIMIT = 2.9 * CLOCKS_PER_SEC;
 
 int n, m;
 vector<string> s;
-vector<string> ans(SZ);
 
-void print_ans() {
-    for (auto &si : ans) cout << si << endl;
-}
+struct Torus {
+    vector<string> val;
+    int c;
+    bitset<1000> found;
+
+    Torus() {
+        val.assign(SZ, string(SZ, '.'));
+        c = 0;
+    }
+
+    void hinsert(int h, int w, string &s, string &original) {
+        original.clear();
+        rep(i, s.size()) {
+            original.push_back(val[h][(w+i)%SZ]);
+            val[h][(w+i)%SZ] = s[i];
+        }
+    }
+
+    void vinsert(int h, int w, string &s, string &original) {
+        original.clear();
+        rep(i, s.size()) {
+            original.push_back(val[(h+i)%SZ][w]);
+            val[(h+i)%SZ][w] = s[i];
+        }
+    }
+
+    bool hfind(int h, string &sub) {
+        rep(w, SZ) {
+            bool res = true;
+            rep(i, sub.size()) {
+                if (val[h][(w+i)%SZ] != sub[i]) {
+                    res = false;
+                    break;
+                }
+            }
+            if (res) return true;
+        }
+        return false;
+    }
+
+    bool vfind(int w, string &sub) {
+        rep(h, SZ) {
+            bool res = true;
+            rep(i, sub.size()) {
+                if (val[(h+i)%SZ][w] != sub[i]) {
+                    res = false;
+                    break;
+                }
+            }
+            if (res) return true;
+        }
+        return false;
+    }
+
+    int score() {
+        int res = 0;
+        rep(i,m) {
+            bool ok = false;
+            rep(j,SZ) {
+                if (hfind(j, s[i]) || vfind(j, s[i])) {
+                    ok = true;
+                    break;
+                }
+            }
+            if (ok) {
+                found.set(i, 1);
+                res++;
+            }
+            else {
+                found.set(i, 0);
+            }
+        }
+        return res;
+    }
+
+    bool exists(int idx) {
+        return found.test(idx);
+    }
+
+    void print() {
+        for (auto &v : val) cout << v << endl;
+    }
+};
 
 int main() {
+    start = clock();
+
     cin >> n >> m;
     s.resize(m);
     rep(i,m) cin >> s[i];
 
-    vector<P> cand;
+    Torus torus;
+    random_device seed_gen;
+    mt19937 engine(seed_gen());
 
-    for (auto &sub: s) {
-        bool find = false;
-        for (auto &[t, cnt] : cand) {
-            if (t.find(sub) != string::npos) {
-                cnt++;
-                find = true;
-                break;
-            }
+    int score = 0;
+    while (true) {
+        rep(i,m) {
+        if (LIMIT < clock() - start) {
+            torus.print();
+            return 0;
         }
-        for (auto &[t, cnt] : cand) {
-            if (sub.find(t) != string::npos) {
-                cnt++;
-                find = true;
-                t = sub;
-                break;
-            }
-        }
-        if (!find) {
-            cand.emplace_back(sub, 1);
-        }
-    }
-
-    sort(cand.begin(), cand.end(), [&](auto x, auto y){
-        return y.second < x.second;
-    });
-
-    for (auto &[ci, cnt] : cand) {
-        rep(i,SZ) {
-            if (ans[i].size() + ci.size() <= SZ) {
-                ans[i] += ci;
-                break;
-            }
+            if (torus.exists(i)) continue;
+            string original = "";
+            string changed = "";
+            int h = engine() % SZ;
+            int w = engine() % SZ;
+            torus.hinsert(h, w, s[i], original);
+            if (chmax(score, torus.score())) continue;
+            torus.hinsert(h, w, original, changed);
+            torus.vinsert(h, w, s[i], original);
+            if (chmax(score, torus.score())) continue;
+            torus.vinsert(h, w, original, changed);
         }
     }
 
-    rep(i,SZ) {
-        ans[i] += string(SZ-ans[i].size(), '.');
-    }
-
-    print_ans();
     return 0;
 }
