643d95f27c28168f4964ca6e36c4ebe8a1f92373
t104 <toshi2005tke@gmail.com>
2021-06-28 15:49:08 +0900

    solved

---
+++ tenkei90/078.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> g(n);
+    rep(i,m) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    int ans = 0;
+    rep(i,n) {
+        int c = 0;
+        for (auto v: g[i]) {
+            if (v < i) c++;
+        }
+        if (c == 1) ans++;
+    }
+    
+    cout << ans << endl;
+    return 0;
+}
