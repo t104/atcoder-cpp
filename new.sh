773ba083dafedc29a81c0d2df215ed869699f3e4
t104 <toshi2005tke@gmail.com>
2021-09-04 11:35:57 +0900

    Make the number of problems variable

--- new.sh
+++ new.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 FOLDER=$1
+UNTILL=$2
 
-files=(
-    "a"
-    "b"
-    "c"
-    "d"
-    "e"
-    "f"
-)
+str="#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+int main() {
+    return 0;
+}
+"
 
 mkdir ./$FOLDER
-for file in "${files[@]}"
+for file in $(eval echo {a..$UNTILL})
 do
-    touch ./$FOLDER/$file.cpp
+    echo "$str" > ./$FOLDER/$file.cpp
 done
