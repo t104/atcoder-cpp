5f30d6342946c7186ebe91301298aaa4df81c020
t104 <toshi2005tke@gmail.com>
2021-09-12 10:58:55 +0900

    after-contest

--- abc218/e.cpp
+++ abc218/e.cpp
@@ -12,8 +12,37 @@ using ll = long long;
 using ld = long double;
 using P = pair<int,int>;
 
+struct Edge {
+    int from, to;
+    ll w;
+    Edge(int from = 0, int to = 0, ll w = 0) : from(from), to(to), w(w) {}
+    bool operator<(Edge &rhs) const {
+        return w < rhs.w;
+    }
+};
+
 int main() {
     int n, m;
+    cin >> n >> m;
+    vector<Edge> edges(m);
+    ll ans = 0;
+    rep(i,m) {
+        int a, b;
+        ll c;
+        cin >> a >> b >> c;
+        --a, --b;
+        ans += c;
+        edges[i] = Edge(a,b,c);
+    }
+    sort(edges.begin(), edges.end());
+    dsu uf(n);
+    for (auto &e: edges) {
+        if (e.w < 0 || !uf.same(e.from, e.to)) {
+            uf.merge(e.from, e.to);
+            ans -= e.w;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
 
