5ee5be0216d41ffeb5f7d4433fde8121789affaa
t104 <toshi2005tke@gmail.com>
2021-09-18 17:11:37 +0900

    resolved

--- abc204/e.cpp
+++ abc204/e.cpp
@@ -16,58 +16,49 @@ struct Edge {
     int to;
     ll c, d;
     Edge(int to, ll c, ll d): to(to), c(c), d(d) {}
-
-    ll dist(ll t) {
-        return t + c + d/(t+1);
+    ll cost (ll t) {
+        return c + d / (t + 1LL);
     }
-
-    ll bestDist(ll t) {
-        ll l = t, r = LINF;
-        if (dist(l) < dist(l+1)) return dist(l);
-        while (l+1 < r) {
-            ll m = (l+r)/2;
-            if (dist(m) > dist(m+1)) l = m;
-            else r = m;
+    ll fastest(ll t) {
+        double rtd = sqrt((double) d);
+        ll res = t + cost(t);
+        for (double d = -2; d <= 2; d += 1.0) {
+            ll start = t + floor(d + rtd);
+            if (t <= start) chmin(res, start + cost(start));
         }
-        return dist(l+1);
+        return res;
     }
 };
 
 vector<vector<Edge>> g;
-vector<ll> dist;
-int n, m;
 
 int main() {
+    int n, m;
     cin >> n >> m;
     g.resize(n);
-    dist.resize(n, LINF);
-    dist[0] = 0;
     rep(i,m) {
-        int a, b, c, d;
+        int a, b;
+        ll c, d;
         cin >> a >> b >> c >> d;
         --a, --b;
         g[a].emplace_back(b, c, d);
         g[b].emplace_back(a, c, d);
     }
-
     priority_queue<P, vector<P>, greater<P>> que;
     que.emplace(0, 0);
-    while (!que.empty()) {
-        auto [d, v] = que.top(); que.pop();
-        if (dist[v] < d) continue;
-        for (auto next: g[v]) {
-            ll nd = next.bestDist(d);
-            if (dist[next.to] <= nd) continue;
-            dist[next.to] = nd;
-            que.emplace(nd, next.to);
+    vector<ll> ans(n, LINF);
+    ans[0] = 0;
+    while (que.size()) {
+        auto [now, v] = que.top(); que.pop();
+        if (ans[v] < now) continue;
+        for (auto e: g[v]) {
+            ll nt = e.fastest(now);
+            if (chmin(ans[e.to], nt)) {
+                que.emplace(nt, e.to);
+            }
         }
     }
-
-    if (dist[n-1] == LINF) {
-        cout << -1 << endl;
-    }
-    else {
-        cout << dist[n-1] << endl;
-    }
+    cout << (ans[n-1] == LINF ? -1 : ans[n-1]) << endl;
     return 0;
+    
 }
