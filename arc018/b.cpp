df4c681c6bae127eceec18f08c46778d783e37b5
t104 <toshi2005tke@gmail.com>
2021-09-26 16:26:02 +0900

    solved

---
+++ arc018/b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> x(n), y(n);
+    rep(i,n) cin >> x[i] >> y[i];
+    int ans = 0;
+    rep(i, n) {
+        for (int j = i+1; j < n; ++j) {
+            for (int k = j+1; k < n; ++k) {
+                ll dx1 = x[j] - x[i];
+                ll dy1 = y[j] - y[i];
+                ll dx2 = x[k] - x[i];
+                ll dy2 = y[k] - y[i];
+                ll s = abs(dx1 * dy2 - dx2 * dy1);
+                if (0 < s && s%2 == 0) ans++;
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+
