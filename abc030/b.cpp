2945a961c7cbbe02dbdc43ff0812b8a9b0edacc4
t104 <toshi2005tke@gmail.com>
2021-10-08 09:37:38 +0900

    not solved

--- abc030/b.cpp
+++ abc030/b.cpp
@@ -12,7 +12,47 @@ using ll = long long;
 using ld = long double;
 using P = pair<int,int>;
 
-int main() {
-    return 0;
+vector<vector<int>> edges;
+vector<int> h, go, back;
+
+int ans;
+
+void dfs(int v, int p, int &hoseki, int &d) {
+    go[v] = d;
+    hoseki += h[v];
+    for (auto nv: edges[v]) {
+        if (nv == p) continue;
+        int hoseki_now = hoseki;
+        int d_now = d;
+        d++;
+        dfs(nv, v, hoseki, d);
+        if (hoseki_now == hoseki) {
+            ans += d - d_now;
+        }
+    }
+    back[v] = d;
+    d++;
 }
 
+int main() {
+    int n, x;
+    cin >> n >> x;
+    --x;
+    h.resize(n);
+    go.resize(n);
+    back.resize(n);
+    edges.resize(n);
+    rep(i,n) cin >> h[i];
+    rep(i,n-1) {
+        int u, v;
+        cin >> u >> v;
+        --u, --v;
+        edges[u].push_back(v);
+        edges[v].push_back(u);
+    }
+    ans = 0;
+    int hoseki = 0, d = 0;
+    dfs(x, -1, hoseki, d);
+    cout << back[x] - go[x] - ans << endl;
+    return 0;
+}
