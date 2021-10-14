1287aed8f7efbfdda128cd1600e569ca802f3f9f
t104 <toshi2005tke@gmail.com>
2021-10-14 16:20:22 +0900

    solved

---
+++ arc035/b.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define INF 1001001001
+#define LINF (1LL<<60)
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){ return a > b && (a = b, true); }
+template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){ return a < b && (a = b, true); }
+template<typename T1,typename T2, typename T3> inline bool in(T1 val, T2 l, T3 r){return l <= val && val < r;}
+using ll = long long;
+using ld = long double;
+using P = pair<int,int>;
+
+using mint = modint1000000007;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> t(n);
+    ll tot = 0;
+    ll now = 0;
+    mint ans = 1;
+    rep(i,n) cin >> t[i];
+    map<ll, int> mp;
+    rep(i,n) {
+        mp[t[i]]++;
+    }
+    for (auto [ti, cnt]: mp) {
+        rep(i, cnt) {
+            now += ti;
+            tot += now;
+            ans *= (i + 1);
+        }
+    }
+    cout << tot << endl;
+    cout << ans.val() << endl;
+    return 0;
+}
