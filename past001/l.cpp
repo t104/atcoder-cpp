68eeb93eb1aeca218455af1d4f498e201da0ec07
t104 <toshi2005tke@gmail.com>
2021-12-29 00:07:39 +0900

    update

--- past001/l.cpp
+++ past001/l.cpp
@@ -12,7 +12,57 @@ using ll = long long;
 using ld = long double;
 using P = pair<int,int>;
 
+int N, M;
+
+struct edge {
+    double c;
+    int from, to;
+    edge(double c, int from, int to) : c(c), from(from), to(to) {}
+    bool operator<(const edge &e) const {
+        return c < e.c;
+    }
+    bool operator>(const edge &e) const {
+        return e.c < c;
+    }
+};
+
 int main() {
+    cin >> N >> M;
+    vector<int> x(N+M), y(N+M), c(N+M);
+    rep(i, N+M) {
+        cin >> x[i] >> y[i] >> c[i];
+    }
+    vector<edge> edges;
+    rep(i, N+M) {
+        for (int j = i + 1; j < N+M; ++j) {
+            int dx = x[i] - x[j];
+            int dy = y[i] - y[j];
+            double d = sqrt(dx*dx + dy*dy);
+            if (c[i] != c[j]) d *= 10;
+            edges.emplace_back(d, i, j);
+        }
+    }
+    sort(edges.begin(), edges.end());
+    auto use = [&](int x, int flg) {
+        if (x < N) return 1;
+        x -= N;
+        return (flg >> x) & 1;
+    };
+    double ans = 1e9;
+    rep(flg, 1<<M) {
+        dsu uf(N + M);
+        double sum = 0;
+        for (auto &e : edges) {
+            if (use(e.from, flg) && use(e.to, flg)) {
+                if (uf.same(e.from, e.to)) continue;
+                uf.merge(e.from, e.to);
+                sum += e.c;
+            }
+        }
+        chmin(ans, sum);
+    }
+    cout << std::fixed << std::setprecision(15);
+    cout << ans << endl;
     return 0;
 }
 
